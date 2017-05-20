b42971cd168ca89551151e3cc14083df011093f1
charlie <charlie00788@hotmail.com>
2017-05-20 09:49:41 -0400

    Tesosreria: cambie algo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel atualizado
+                    Laravel atualizado ghfh
                 </div>
 
                 <div class="links">
